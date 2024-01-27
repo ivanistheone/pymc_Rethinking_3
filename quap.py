0f498e7abbb43d41999f5e0a1aba416c066d2ce3
Ivan Savov <ivan.savov@gmail.com>
2024-01-27 14:04:39 -0500

    Use transformed var value when calling find_hessian

--- quap.py
+++ quap.py
@@ -36,10 +36,26 @@ def quap(vars, n_samples=10_000):
     """
     map = pm.find_MAP(vars=vars, method="BFGS")
 
+    # We need to remove transform from the vars in PyMC v5
+    # to get the correct uncertainties for tansforemd variables
+    # via https://github.com/pymc-devs/pymc/issues/5443#issuecomment-1030609090
+    m = pm.model.core.modelcontext(None)
+    for var in vars:
+        if m.rvs_to_transforms[var] is not None:
+            m.rvs_to_transforms[var] = None
+            # change name so that we can use `map[var]` value
+            var_value = m.rvs_to_values[var]
+            var_value.name = var.name
+
+    # Find Hessian and invert it to get the covariance matrix
     H = pm.find_hessian(map, vars=vars)
     cov = np.linalg.inv(H)
+
+    # Build posterior
     mean = np.concatenate([np.atleast_1d(map[v.name]) for v in vars])
     posterior = scipy.stats.multivariate_normal(mean=mean, cov=cov)
+
+    # Sample from the posterior
     draws = posterior.rvs(n_samples)[np.newaxis, ...]
     if draws.ndim == 2:
         draws = draws[..., np.newaxis]
@@ -52,4 +68,3 @@ def quap(vars, n_samples=10_000):
             samples[v.name] = samples[v.name].squeeze(axis=-1)
         i += var_size
     return az.convert_to_inference_data(samples)
-
